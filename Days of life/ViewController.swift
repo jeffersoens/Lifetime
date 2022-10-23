43fe86e235e66d02baa25a1885a13ff196b3f9ed
Vyacheslav Gorbatenko <jeffersoens@gmail.com>
2022-10-23 15:59:26 +0300

    Initial Commit

---
+++ Days of life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Days of life
+//
+//  Created by Вячеслав Горбатенко on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
